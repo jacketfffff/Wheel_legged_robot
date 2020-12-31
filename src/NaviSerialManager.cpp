41cae4681baf38fc0bbe7469486ef40e0361de41
jacketfffff <1095117186@qq.com>
2020-12-31 15:40:30 +0800

    test update code

--- src/NaviSerialManager.cpp
+++ src/NaviSerialManager.cpp
@@ -1,6 +1,6 @@
 #include "basecontrol/NaviSerialManager.h"
 #include "ros/ros.h"
-NaviSerialManager::NaviSerialManager(std::string serial_addr, unsigned int baudrate):SerialManager(serial_addr,baudrate)
+NaviSerialManager::NaviSerialManager(std::string serial_addr, unsigned int baudrate,int command_size = 8):SerialManager(serial_addr,baudrate)
 {
 
 }
@@ -17,7 +17,7 @@ NaviSerialManager::~NaviSerialManager()
     }
 }
 void NaviSerialManager::registerAutoReadThread(int rate)
-{
+{   
     thread_ptr_.reset(new boost::thread(boost::bind(&NaviSerialManager::readWorker, this, rate)));
     isAutoThreadRegistered_=true;
 }
@@ -46,8 +46,33 @@ int NaviSerialManager::getCommandBeginIndex(int check_begin_index)
 {
     for(int i=0;i<read_used_bytes-check_begin_index;++i)
     {
-        if(read_buffer[i+check_begin_index]==COMMAND_HEAD)
-            return  i+check_begin_index;
+        //if(read_buffer[i+check_begin_index]==COMMAND_HEAD || read_buffer[i+check_begin_index]==KNEE_HEAD || read_buffer[i+check_begin_index]==ANKLE_HEAD)
+        if(read_buffer[i+check_begin_index] == COMMAND_HEAD)
+        {
+            if(read_buffer[i+check_begin_index+8] == COMMAND_TAIL)
+            {
+                command_size = 8;
+                return  i+check_begin_index;
+            }
+                //这里是要对每包数据进行二次检测
+        }
+        else if (read_buffer[i+check_begin_index] == FORCESENSOR_HEAD)
+        {
+            if(read_buffer[i+check_begin_index+12] == FORCESENSOR_TAIL)
+            { 
+                command_size = 12;
+                return  i+check_begin_index;
+            }        
+        }
+        else if (read_buffer[i+check_begin_index] == ENCODER_HEAD)
+        {
+            if (read_buffer[i+check_begin_index+12] == ENCODER_TAIL)
+            {
+                command_size = 12; 
+                return i+check_begin_index;
+            }
+        }
+        //return  i+check_begin_index;
     }
     return read_used_bytes;
 }
@@ -61,23 +86,23 @@ void NaviSerialManager::receive()
     {
         serial_alive_ =true;
         read_used_bytes +=receiveNumbers;
-        if(read_used_bytes>=COMMAND_SIZE)
+        if(read_used_bytes>=command_size)
         {
             int commandBeginIndex{0};
             ReadResult temp{};
-            for(int i=0;i<read_used_bytes;i+=COMMAND_SIZE)
+            for(int i=0;i<read_used_bytes;i+=command_size)
             {
                 commandBeginIndex=getCommandBeginIndex(commandBeginIndex+i);
-                if(commandBeginIndex<=read_used_bytes-COMMAND_SIZE)
+                if(commandBeginIndex<=read_used_bytes-command_size)
                 {
-                    memcpy(&temp.read_result[i], &read_buffer[commandBeginIndex], COMMAND_SIZE);
-                    temp.read_bytes += COMMAND_SIZE;
+                    memcpy(&temp.read_result[i], &read_buffer[commandBeginIndex], command_size);
+                    temp.read_bytes += command_size;
                 }
                 else
                    break;
             }
             read_result_queue.push(temp);
-            if(commandBeginIndex<read_used_bytes&&commandBeginIndex>read_used_bytes-COMMAND_SIZE)
+            if(commandBeginIndex<read_used_bytes&&commandBeginIndex>read_used_bytes-command_size)
             {
                 char transfer_buffer[read_used_bytes-commandBeginIndex]{};
                 memcpy(&transfer_buffer[0],&read_buffer[commandBeginIndex],read_used_bytes-commandBeginIndex);
@@ -89,10 +114,11 @@ void NaviSerialManager::receive()
             {
                 memset(read_buffer,0,BUFFER_SIZE);
                 read_used_bytes=0;
+                司法的发送
             }
         }
         //Warning, ignore some data when the STM32 send wrong many times
-        if(read_used_bytes>=BUFFER_SIZE-COMMAND_SIZE)
+        if(read_used_bytes>=BUFFER_SIZE-command_size)
         {
             read_used_bytes=0;
             memset(read_buffer,0,BUFFER_SIZE);
